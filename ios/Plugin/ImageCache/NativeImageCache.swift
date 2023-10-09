e74f67fe96a67b700320f951fa3e95e8e74c8662
Tony Myers <tony@myers.co.nz>
2023-10-10 09:19:58 +1300

    Support for SVG markers

--- ios/Plugin/ImageCache/NativeImageCache.swift
+++ ios/Plugin/ImageCache/NativeImageCache.swift
@@ -1,8 +1,10 @@
 import SDWebImage
+import SVGKit
 
 final class NativeImageCache: ImageURLLoadable {
     static let shared: ImageURLLoadable = NativeImageCache()
 
+    
     private lazy var cache: NSCache<AnyObject, AnyObject> = {
         NSCache<AnyObject, AnyObject>()
     }()
@@ -10,32 +12,73 @@ final class NativeImageCache: ImageURLLoadable {
     private init(){}
 
     func image(at urlString: String, resizeWidth: Int, resizeHeight: Int, completion: @escaping VoidReturnClosure<UIImage?>) {
-        guard let url = URL(string: urlString) else {
-            completion(nil)
-            return
+        // Check if the provided string is a data URL.
+        if urlString.starts(with: "http:") || urlString.starts(with: "https:") {
+            if let url = URL(string: urlString) {
+                // If urlString starts with "http:" or "https:" and can be converted to a valid URL, download and process the image.
+                downloadImage(fromURL: url, resizeWidth: resizeWidth, resizeHeight: resizeHeight, completion: completion)
+            } else {
+                // Handle the case where urlString is not a valid URL.
+                completion(nil)
+            }
+        } else {
+            if let svgData = Data(base64Encoded: urlString, options: Data.Base64DecodingOptions(rawValue: 0)) {
+                processSVGImage(svgData, resizeWidth: resizeWidth, resizeHeight: resizeHeight, completion: completion)
+            } else {
+                // Handle the case where the input is not valid SVG data.
+                completion(nil)
+            }
         }
-        
+        // Handle the case where the input is neither a data URL nor a valid URL.
+        // print("Invalid image")
+        // completion(nil)
+    }
+
+    private func downloadImage(fromURL url: URL, resizeWidth: Int, resizeHeight: Int, completion: @escaping VoidReturnClosure<UIImage?>) {
         // Generate custom key based on the size,
         // so we can cache the resized variant of the image as well.
-        let key = "\(urlString)\(resizeWidth)\(resizeHeight)"
-        
+        let key = "\(url.absoluteString)\(resizeWidth)\(resizeHeight)"
+
         if let image = cache.object(forKey: key as AnyObject) as? UIImage {
-            // If the resized image is found in the cache,
-            // return it.
+            // If the resized image is found in the cache, return it.
             completion(image)
         } else {
-            // Otherwise, we should download the original image,
+            // Otherwise, download the original image.
             SDWebImageDownloader.shared.downloadImage(with: url, options: [], context: nil, progress: nil) { image, _, _, _ in
-                // then resize it to the preferred size,
+                // Resize the downloaded image to the preferred size.
                 guard let resizedImage = image?.resize(targetSize: CGSize(width: resizeWidth, height: resizeHeight)) else {
                     completion(nil)
                     return
                 }
-                // save it in the cache,
+                // Save the resized image in the cache.
                 self.cache.setObject(resizedImage, forKey: key as AnyObject)
-                // and return it.
+                // Return the resized image.
                 completion(resizedImage)
             }
+        }
+    }
+
+    private func processSVGImage(_ svgData: Data, resizeWidth: Int, resizeHeight: Int, completion: @escaping VoidReturnClosure<UIImage?>) {
+        // Generate a custom key based on the size and SVG data.
+        let key = "\(svgData.count)\(resizeWidth)\(resizeHeight)"
+
+        if let image = cache.object(forKey: key as AnyObject) as? UIImage {
+            // If the resized image is found in the cache, return it.
+            completion(image)
+        } else {
+            guard let svgImage = SVGKImage(data: svgData) else {
+                completion(nil)
+                return
+            }
+
+            // Resize the SVG image
+            svgImage.size = CGSize(width: CGFloat(resizeWidth), height: CGFloat(resizeHeight))
+
+            // Convert the SVG image to a UIImage
+            let uiImage = svgImage.uiImage
+
+            // Return the resized UIImage
+            completion(uiImage)
         }
     }
 
